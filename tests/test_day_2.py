08f7b7dc1ffff5070cd1b1d5d948d3b0e03faa60
joaooliveira247 <joaooliveira247@gmail.com>
2023-12-12 12:22:44 -0300

    :white_check_mark: add test to day 2 part 2

--- tests/test_day_2.py
+++ tests/test_day_2.py
@@ -1,4 +1,11 @@
-from aoc_2023.day_2 import valid_game, sep_num, sep_games, is_valid_match
+from aoc_2023.day_2 import (
+    valid_game,
+    sep_num,
+    sep_games,
+    is_valid_match,
+    larger_values,
+    mult_list,
+)
 
 
 def test_valid_game() -> None:
@@ -37,11 +44,33 @@ def test_is_valid_match() -> None:
     ]
     assert is_valid_match(test_game) == True
 
+
 def test_is_invalid_match() -> None:
     test_game: list[dict[str, int]] = [
-            {"blue": 3, "red": 4},
-            {"red": 19, "green": 2, "blue": 6},
-            {"green": 27},
-        ]
-    
+        {"blue": 3, "red": 4},
+        {"red": 19, "green": 2, "blue": 6},
+        {"green": 27},
+    ]
+
     assert is_valid_match(test_game) == False
+
+
+def test_larger_values() -> None:
+    test_game: str = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green"
+    assert larger_values(test_game) == [4, 2, 6]
+
+
+def test_mult_list() -> None:
+    test_values: list[int] = [2, 4, 6]
+    assert mult_list(test_values) == 48
+
+
+def test_day_2() -> None:
+    input: list[str] = [
+        "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green",
+        "Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue",
+        "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red",
+        "Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red",
+        "Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green",
+    ]
+    assert sum([mult_list(larger_values(i)) for i in input]) == 2286
